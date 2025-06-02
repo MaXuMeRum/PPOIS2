ac79ca16ca2d8daef57608a37ac495f8397d9df6
MaXuMeRum <144938023+MaXuMeRum@users.noreply.github.com>
2025-06-02 22:37:50 +0300

    Delete lr1/LR1/run_all_tests.py

--- lr1/LR1/run_all_tests.py
+++
@@ -1,8 +0,0 @@
-﻿import unittest
-
-if __name__ == '__main__':
-    loader = unittest.TestLoader()
-    suite = loader.discover(start_dir='tests', pattern='test_*.py')
-    runner = unittest.TextTestRunner()
-    runner.run(suite)
-
